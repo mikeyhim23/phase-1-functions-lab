d22b4f80f6ecb1a98caafd6a1a50565f3c323f64
christopher karanja nganga <christopher.nganga@student.moringaschool.com>
2024-09-30 11:48:27 +0300

    Fuctions lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; 
+    return Math.abs(pickupLocation - hqLocation);
+}
+
+
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return distanceInBlocks * feetPerBlock;
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    const freeFeet = 400;
+    const standardRate = 0.02;
+    const maxDistance = 2500;
+    const flatRate = 25; 
+
+    if (distanceInFeet <= freeFeet) {
+        return 0;
+    } else if (distanceInFeet > freeFeet && distanceInFeet <= 2000) {
+        return (distanceInFeet - freeFeet) * standardRate;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= maxDistance) {
+        return flatRate; 
+    } else {
+        return 'cannot travel that far'; 
+    }
+}
